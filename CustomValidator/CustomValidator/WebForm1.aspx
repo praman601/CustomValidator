0df71c0ead7b92fe26f8aa5787456c605735fbb7
pradeep <pradeep.budhathoki1234@gmail.com>
2017-09-14 13:41:45 +0545

    another commit

---
+++ CustomValidator/CustomValidator/WebForm1.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="CustomValidator.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table>
+                <tr>
+                    <td>
+                        <b>Enter a positive even number:</b>
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtEven" runat="server"></asp:TextBox>
+                        <asp:CustomValidator ID="txtCustomValidator" runat="server" 
+                            ErrorMessage="Not an even Number"
+                            ForeColor="Red"
+                            ControlToValidate="txtEven" OnServerValidate="txtCustomValidator_ServerValidate"
+                            
+                            ></asp:CustomValidator>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <asp:Button ID="btnSubmit" runat="server" Text="Submit" EnableTheming="False" OnClick="btnSubmit_Click" />
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <asp:Label ID="lblStatus" runat="server"></asp:Label>
+                    </td>
+                </tr>
+
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ CustomValidator/CustomValidator/WebForm1.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CustomValidator
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ValidationSettings.UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
+        }
+
+        protected void txtCustomValidator_ServerValidate(object source, ServerValidateEventArgs args)
+        {
+            if (args.Value == "")
+            {
+                args.IsValid = false;
+            }
+            else
+            {
+                int number;
+                bool isnumber = int.TryParse(args.Value, out number);
+
+
+                if (isnumber && number>0 && number % 2 == 0)
+                {
+                    args.IsValid = true;
+                }
+                else
+                {
+                    args.IsValid = false;
+                }
+            }
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            
+            if(Page.IsValid)
+            {
+                lblStatus.Text = "Data Saved";
+                lblStatus.ForeColor = System.Drawing.Color.Green;
+            }
+            else
+            {
+                lblStatus.Text = "Data not Saved";
+                lblStatus.ForeColor = System.Drawing.Color.Red;
+            }
+        }
+    }
+}
